0648d2819c84371faf1583aedc4e44dc7faeab06
Eugene Cheltsov <chill.icp@gmail.com>
2014-05-08 14:46:25 +0400

    first

---
+++ tests/fixtures/js/transform.js
@@ -0,0 +1,8 @@
+/**
+ * @jsx React.DOM
+ */
+React.DOM.div( {className:"test"}, 
+  React.DOM.div( {className:"test_value"}, this.props.testValue),
+  React.DOM.div( {className:"test_name"}, this.props.testName)
+)
+;
---
+++ tests/fixtures/jsx/transform.jsx
@@ -0,0 +1,7 @@
+/**
+ * @jsx React.DOM
+ */
+<div className="test">
+  <div className="test_value">{this.props.testValue}</div>
+  <div className="test_name">{this.props.testName}</div>
+</div>
